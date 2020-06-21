5d8aa5451af18d3fedb6bfa58edeb59915315eb8
Rahul Haque <rahulhaque07@gmail.com>
2020-06-21 13:59:51 +0600

    Build added for github pages

---
+++ build/precache-manifest.3002c5ce158447f31a94202dfc4320a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab96fe22cac81f69da95c60df936bab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ab556e0a4107ead237d",
+    "url": "./static/css/2.7fb08d59.chunk.css"
+  },
+  {
+    "revision": "4f85606938675f87da22",
+    "url": "./static/css/main.c307118f.chunk.css"
+  },
+  {
+    "revision": "2ab556e0a4107ead237d",
+    "url": "./static/js/2.4e742ba6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.4e742ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f85606938675f87da22",
+    "url": "./static/js/main.c8bd8dfa.chunk.js"
+  },
+  {
+    "revision": "f98c523b50595ac929b0",
+    "url": "./static/js/runtime-main.07971bfb.js"
+  }
+]);
